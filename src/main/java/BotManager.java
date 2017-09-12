986b64a6974c8689afcee0baa12a2dd506053602
Ratmir-Agliullin <ratmir-91@mail.ru>
2017-09-12 21:49:57 +0400

    ver.0.1

--- src/main/java/BotManager.java
+++ src/main/java/BotManager.java
@@ -1,5 +1,9 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import static java.awt.DefaultKeyboardFocusManager.sendMessage;
 
 /**
  * Created by Аглиуллины on 12.09.2017.
@@ -18,5 +22,24 @@ public class BotManager extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "367015082:AAHDEcfBder_4Z_oY5LWygwa3qtJlWhzE28";
+    }
+
+    public void SendText(Long chat_id,  String out){
+
+
+
+        SendMessage message = new SendMessage().setChatId(chat_id).setText(out);
+
+
+        try {
+
+            sendMessage(message); // Sending our message object to user
+
+        } catch (TelegramApiException e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 }
---
+++ src/main/java/BotService.java
@@ -0,0 +1,12 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import static java.awt.DefaultKeyboardFocusManager.sendMessage;
+
+/**
+ * Created by Аглиуллины on 12.09.2017.
+ */
+public class BotService {
+
+
+}
